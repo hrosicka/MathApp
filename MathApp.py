0c8ab8a09c1074f8581848f710625e9763785613
Hanka <hrosicka@post.cz>
2023-12-06 16:48:01 +0100

    Code clearing

--- MathApp.py
+++ MathApp.py
@@ -48,21 +48,6 @@ class MplCanvas(FigureCanvasQTAgg):
         self.axes.set_aspect('equal', adjustable='box')
         super(MplCanvas, self).__init__(fig)
 
-class AnotherWindow(QWidget):
-    """
-    This "window" is a QWidget. If it has no parent,
-    it will appear as a free-floating window.
-    """
-
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout()
-        self.label = QLabel("Another Window % d" % randint(0, 100))
-        layout.addWidget(self.label)
-        sc = MplCanvas(self, width=5, height=5, dpi=100)
-        layout.addWidget(sc)
-        self.setLayout(layout)
-
 
 class MainWindow(QMainWindow):
 
