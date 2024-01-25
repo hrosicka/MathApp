5d7a6443376110faa4afaad915e5bf379d9c3fec
Hanka <hrosicka@post.cz>
2024-01-25 21:50:56 +0100

    Square Form validator added

--- Square.py
+++ Square.py
@@ -121,6 +121,7 @@ class WindowSquare(QWidget):
         layout_param.addWidget(self.label_centerX,1,0)
 
         self.edit_centerX = QLineEdit(self)
+        self.edit_centerX.setValidator(validator_double)
         self.edit_centerX.setAlignment(QtCore.Qt.AlignRight)
         self.edit_centerX.setFixedWidth(150)
         layout_param.addWidget(self.edit_centerX,1,1)
@@ -136,6 +137,7 @@ class WindowSquare(QWidget):
         layout_param.addWidget(self.label_centerY,2,0)
 
         self.edit_centerY = QLineEdit(self)
+        self.edit_centerY.setValidator(validator_double)
         self.edit_centerY.setAlignment(QtCore.Qt.AlignRight)
         self.edit_centerY.setFixedWidth(150)
         layout_param.addWidget(self.edit_centerY,2,1)
@@ -195,6 +197,15 @@ class WindowSquare(QWidget):
         self.label_dim_area.setFixedWidth(30)
         layout_res.addWidget(self.label_dim_area,1,2)
 
+        self.edit_side.textChanged.connect(self.check_state_side)
+        self.edit_side.textChanged.emit(self.edit_side.text())
+
+        self.edit_centerX.textChanged.connect(self.check_state_centerX)
+        self.edit_centerX.textChanged.emit(self.edit_centerX.text())
+
+        self.edit_centerY.textChanged.connect(self.check_state_centerY)
+        self.edit_centerY.textChanged.emit(self.edit_centerY.text())
+
 
 
     def plot_square(self, square_plot, square_color):
@@ -232,5 +243,49 @@ class WindowSquare(QWidget):
         self.edit_centerY.clear()
         self.label_res_area.setText("0.0")
         self.label_res_perimeter.setText("0.0")
+
+    def check_state_side(self, *args, **kwargs):
+        sender = self.sender()
+        validator = sender.validator()
+        state = validator.validate(sender.text(), 0)[0]
+        if self.edit_side.text() == "0" or self.edit_side.text() == "":
+            color = '#f6989d' # red
+        elif state == QValidator.Acceptable:
+            color = '#c4df9b' # green
+        elif state == QValidator.Intermediate:
+            color = '#fff79a' # yellow
+        else:
+            color = '#f6989d' # red
+        sender.setStyleSheet('QLineEdit { background-color: %s }' % color)
+
+
+    def check_state_centerX(self, *args, **kwargs):
+        sender = self.sender()
+        validator = sender.validator()
+        state = validator.validate(sender.text(), 0)[0]
+        if self.edit_centerX.text() == "":
+            color = '#f6989d' # red
+        elif state == QValidator.Acceptable:
+            color = '#c4df9b' # green
+        elif state == QValidator.Intermediate:
+            color = '#fff79a' # yellow
+        else:
+            color = '#f6989d' # red
+        sender.setStyleSheet('QLineEdit { background-color: %s }' % color)   
+
+
+    def check_state_centerY(self, *args, **kwargs):
+        sender = self.sender()
+        validator = sender.validator()
+        state = validator.validate(sender.text(), 0)[0]
+        if self.edit_centerY.text() == "":
+            color = '#f6989d' # red
+        elif state == QValidator.Acceptable:
+            color = '#c4df9b' # green
+        elif state == QValidator.Intermediate:
+            color = '#fff79a' # yellow
+        else:
+            color = '#f6989d' # red
+        sender.setStyleSheet('QLineEdit { background-color: %s }' % color) 
 
         
