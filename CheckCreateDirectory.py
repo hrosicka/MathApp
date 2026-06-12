68d9f6303ffcd74398186a2c22d0ea796d3baa34
Hanka <hrosicka@post.cz>
2026-06-12 19:47:04 +0200

    style: format CheckCreateDirectory.py with black

--- CheckCreateDirectory.py
+++ CheckCreateDirectory.py
@@ -1,10 +1,11 @@
 import os
 
+
 # Check whether the specified path exists or not
 def check_create_dir(path):
 
     isExist = os.path.exists(path)
-    
+
     if not isExist:
 
         # Create a new directory because it does not exist
@@ -12,4 +13,4 @@ def check_create_dir(path):
         print("The new directory is created!")
 
     else:
-         print("The directory already exists")
+        print("The directory already exists")
